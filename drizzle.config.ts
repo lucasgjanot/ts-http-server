29f6a378686a97bfb1001479dd5c83ec0ad6067a
lucasgjanot <lgjanot402@gmail.com>
2025-10-24 00:01:47 -0300

    Add dbURL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "./src/config.js";
+
 
 export default defineConfig({
   schema: "src/lib/db/schema.ts",
   out: "src/lib/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: ,
+    url: config.dbURL
   },
 });
